15bb326809ec28790478ace482f18949db672df7
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-23 12:40:44 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,10 @@
 const itemEl = document.querySelectorAll('.item');
-console.log(`1) Number of categories: ${itemEl.length}.`);
+console.log(`Number of categories: ${itemEl.length}.`);
 
-itemEl.forEach(element => {
-  console.log(`Category: ${element.querySelector('h2').textContent}`);
-  console.log(`Elements: ${element.querySelector('ul').children.length}`);
-  console.log(` `);
-});
+console.log(
+  itemEl.forEach(element => {
+    console.log(`Category: ${element.querySelector('h2').textContent}`);
+    console.log(`Elements: ${element.querySelector('ul').children.length}`);
+    console.log(` `);
+  })
+);
