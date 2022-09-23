1fd2def3d93aa9febd40db8e2a509b4fb4427ad4
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-23 12:47:52 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingridientList = [];
+
+ingredients.forEach(igridient => {
+  const item = document.createElement(`li`);
+  item.textContent = igridient;
+  item.classList.add('item');
+  ingridientList.push(item);
+});
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.append(...ingridientList);
