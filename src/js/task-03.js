c15fe5f134d4ebb2062319ec7c4ab023d52a54c3
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-24 23:23:19 +0300

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector('.gallery');
+
+const imgList = images.map(img => `<li><img src='${img.url}' alt='${img.alt}'/></li>`).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', imgList);
