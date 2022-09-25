ef13269f735b5537b91d41d323db0da7c3662901
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-26 00:55:18 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,35 @@
+// 1) Обработка отправки формы form.login-form должна быть по событию submit.
+// 2) При отправке формы страница не должна перезагружаться.
+// 3)Если в форме есть незаполненные поля, выводи alert с
+// предупреждением о том, что все поля должны быть заполнены.
+//  4)Если пользователь заполнил все поля и отправил форму,
+//  собери значения полей в обьект, где имя поля будет именем свойства,
+//     а значение поля - значением свойства.
+//     Для доступа к элементам формы используй свойство elements.
+// 5)Выведи обьект с введенными данными в консоль и очисти
+// значения полей формы методом reset.
 
+const submitForm = document.querySelector(`.login-form`);
+
+submitForm.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  // Reset to default
+  event.preventDefault();
+  //make inputs ref
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('All field in form must be completed');
+  } else {
+    const output = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(output);
+    // Reset form
+    event.currentTarget.reset();
+  }
+}
