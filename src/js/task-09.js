8d1f72085ed70025356657bf810e69f1d3b27748
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-26 00:59:26 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+function colorChange() {
+  color.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+btn.addEventListener('click', colorChange);
