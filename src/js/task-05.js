4f4f5c15a933b77d919c4b9cfac67059a82fd955
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-09-25 13:17:17 +0300

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,10 +1,12 @@
 const inputPlace = document.querySelector(`#name-input`);
 const outputName = document.querySelector(`#name-output`);
 
-inputPlace.addEventListener('input', event => {
+function changeName(event) {
   if (event.currentTarget.value === '') {
     outputName.textContent = 'Anonymous';
   } else {
     outputName.textContent = event.currentTarget.value;
   }
-});
+}
+
+inputPlace.addEventListener('input', changeName);
